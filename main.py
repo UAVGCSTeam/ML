85428e7daddeec776b0de28c1b5fb03846a6053e
Carlos <carlosfvargas526@gmail.com>
2024-11-15 17:20:27 -0800

    very very small changes

--- main.py
+++ main.py
@@ -7,32 +7,33 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description = 'YoloV8 Fire Detection')
     parser.add_argument(
         "--webcam-resolution",
-        # 1280,720
-        default=[1280,720],
+        default=[1280,720], # originally 1280,720
         type =int,
         nargs= 2
     )
     parser.add_argument(
         '--model',   
         # default= r"model/fire_detection.onnx", 
-        # default= r"model/fire_detection.pt", # shittier
+        default= r"model/fire_detection.pt", # shittier
         # default= r"model/fireV2.onnx", 
-        default= r"model/fireV2.pt", 
+        # default= r"model/fireV2.pt", 
         type=str
     )
     parser.add_argument(
         "--source", 
         # type=str
+        # default="test_folder/test_img.jpg",
+        # default="test_folder/thermal_test.jpg",
         # default="test_folder/test_video.mp4",
-        # default="test_folder/test_video2.mp4",
+        # default="test_folder/test_video2.mp4", # Super far away - most likely not pracitcal
         # default="test_folder/test_video3.mp4",
         # default="test_folder/test_video4.mp4",
-        # default="test_folder/test_video5.mp4",
+        # default="test_folder/test_video5.mp4", # tractor in the video
         # default="test_folder/test_video6.mp4",
         # default="test_folder/test_video7.mp4",
-        default="test_folder/test_video8.mp4",
+        default="test_folder/test_video8.mp4", # clear fire line
         # default="test_folder/test_video9.mp4",
-        # default="test_folder/test_video10.mp4",
+        # default="test_folder/test_video10.mp4", # tree on fire
         # default=0, # accesses the webcam of your computer 
         help="Path to the input video file",
         type=str
@@ -63,10 +64,10 @@ def main():
         results = fire_model.inference(frame) # where the actual inference happens
         for result in results:
             x1, y1, x2, y2, conf, cls = result
-            print(str(int(x1)) + " " + str(int(x2)) + " " + str(int(y1)) + " " + str(int(y2)))
+            print("BB Coords: " + str(int(x1)) + " " + str(int(x2)) + " " + str(int(y1)) + " " + str(int(y2)))
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
             cv2.putText(frame, str(fire_model.names[int(cls)]), (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        frame = cv2.resize(frame, (640, 640))  
+        # frame = cv2.resize(frame, (640, 640)) # original at 640 x 640  
         cv2.imshow("frame", frame)
 
         # press Esc to close the window
